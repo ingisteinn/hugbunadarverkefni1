4791f7460d9d6fbe7861d36b031c5f8c76331cb6
gummigud <gudmundurg1996@gmail.com>
2018-11-11 14:25:09 +0000

    usergaman

--- src/main/java/project/service/UserService.java
+++ src/main/java/project/service/UserService.java
@@ -1,15 +1,25 @@
 package project.service;
 
 import org.springframework.stereotype.Service;
+import project.persistence.entities.Exercise;
 import project.persistence.entities.User;
+import project.persistence.repositories.ExerciseRepository;
+import project.persistence.repositories.UserRepository;
 
 @Service
 public class UserService {
-    public UserService() {
 
-    }
+    UserRepository repository;
 
     public User save(User user) {
-        return null;
+        return repository.save(user);
     }
+
+    public User findOne(String username) {
+        return repository.findOne(username);
+    }
+
+ 
+
+
 }
