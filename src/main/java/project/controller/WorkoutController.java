2a696f72c2606d3db628268f5039a2dc2c0b0c77
Pattihrafn22 <35534176+Pattihrafn22@users.noreply.github.com>
2018-11-27 21:58:35 +0000

    Workout dóterí

--- src/main/java/project/controller/WorkoutController.java
+++ src/main/java/project/controller/WorkoutController.java
@@ -3,12 +3,16 @@ package project.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import project.persistence.entities.Exercise;
 import project.persistence.entities.Workout;
 import project.service.ExerciseService;
 import project.service.UserService;
 import project.service.WorkoutService;
+
+import java.util.List;
 
 @Controller
 public class WorkoutController {
@@ -17,8 +21,10 @@ public class WorkoutController {
     private UserService userService;
 
     @Autowired
-    public WorkoutController(WorkoutService workoutService) {
+    public WorkoutController(WorkoutService workoutService, ExerciseService exerciseService, UserService userService) {
         this.workoutService = workoutService;
+        this.exerciseService = exerciseService;
+        this.userService = userService;
     }
 
     /*
@@ -27,11 +33,28 @@ public class WorkoutController {
 
     @RequestMapping(value = "/workout", method = RequestMethod.GET)
     public String workoutViewGet(Model model) {
+        model.addAttribute("workout", new Workout());
+        model.addAttribute("exercise", new Exercise());
+        model.addAttribute("exercises", exerciseService.findAll());
         return "Workout";
     }
 
     @RequestMapping(value = "/workout", method = RequestMethod.POST)
-    public String workoutViewGet(Workout workout, Model model) {
+    public String workoutViewPost(Workout workout, Model model) {
+        this.workoutService.save(workout);
+
+        model.addAttribute("exercises", exerciseService.findAll());
         return "Workout";
+    }
+
+    @RequestMapping(value = "/addExerciseToWorkout", method = RequestMethod.POST)
+    public String workoutViewPostExercise(@ModelAttribute("exercise") Exercise ex, @ModelAttribute("workout") Workout workout, Model model) {
+        List<Exercise> listEx = workout.getExercises();
+        listEx.add(ex);
+        workout.setExercises(listEx);
+
+        model.addAttribute("workout", workout);
+        model.addAttribute("exercises", exerciseService.findAll());
+        return "redirect:workout";
     }
 }
--- src/main/webapp/WEB-INF/jsp/Workout.jsp
+++ src/main/webapp/WEB-INF/jsp/Workout.jsp
@@ -20,14 +20,45 @@
         <c:when test="${empty sessionScope.login}">
             <h1>You have to be logged in to view your workouts!</h1>
         </c:when>
-
-        <%--If the session has a login attribute--%>
         <c:otherwise>
+        <%--If the session has a login attribute--%>
+
+            <!-- Hér kemur listi af workouts sem eru til -->
+            <h2> Create a new workout: </h2>
+            <sf:form method="POST" modelAttribute="exercise" action="/workout">
+                <div>
+                    <label for="exercise">Exercise: </label>
+                    <sf:select  id="exercise" path="id">
+                        <c:forEach items="${exercises}" var="exercise">
+                            <sf:option value="${exercise.id}">
+                                ${exercise.name}
+                            </sf:option>
+                        </c:forEach>
+                    </sf:select>
+                </div>
+
+                <div>
+                    <label for="name">Name: </label>
+                    <sf:input id="name" path="name" type="text" required="required"/>
+                </div>
+                <div>
+                    <label for="category">Category: </label>
+                    <sf:input id="category" path="category" type="text"/>
+                </div>
+                <div>
+                    <label for="sets">Sets: </label>
+                    <sf:input id="sets" path="sets" type="number" required="required"/>
+                </div>
+
+            </sf:form>
+
             <!-- Hér kemur listi af workouts, titill sem hægt er að ýta á -->
 
-            <div class="add__item">
-                <a class="button" href="/workout/create">Create a new workout</a>
-            </div>
+
+
+        <div class="add__item">
+            <a class="button" href="/workout/create">Create a new workout</a>
+        </div>
 
         </c:otherwise>
     </c:choose>
